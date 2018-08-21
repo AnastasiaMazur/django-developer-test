34b53cb53fa2960550e5672bb0c18500ce307fc5
Anastasia Mazur <anastasia.mazur96@gmail.com>
2018-08-21 09:48:16 +0300

    added roadmap/urls.py

--- roadmap/urls.py
+++ roadmap/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls import patterns, url
 
+from rest_framework.routers import DefaultRouter
+
+from roadmap.views import APIViewSet
+
+router = DefaultRouter()
+router.register(r'api/projects', APIViewSet)
 
 urlpatterns = patterns(
     'roadmap.views',
-    url(r'^$', 'dashboard', name='dashboard')
+    url(r'^$', 'dashboard', name='dashboard'),
 )
+
+urlpatterns += router.urls
